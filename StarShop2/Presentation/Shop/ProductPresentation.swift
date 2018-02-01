094fa640ecca4ab3b9d3d26e31d691f63b9d38b7
Tatiana <tatimagdalena@gmail.com>
2018-01-31 22:56:13 -0200

    Add the views product model

---
+++ StarShop2/Presentation/Shop/ProductPresentation.swift
@@ -0,0 +1,15 @@
+//
+//  ProductPresentation.swift
+//  StarShop2
+//
+//  Created by Tatiana Magdalena on 30/01/18.
+//  Copyright © 2018 TOM. All rights reserved.
+//
+
+import Foundation
+
+struct ProductPresentation {
+    var titleText: String
+    var priceText: String
+    var imageUrl: String
+}
