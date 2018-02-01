b7610d9bca7a214a7a741cad28c5c18d5918857d
Tatiana <tatimagdalena@gmail.com>
2018-01-31 22:51:52 -0200

    Add product data model

---
+++ StarShop2/Data/Model/Product.swift
@@ -0,0 +1,20 @@
+//
+//  Product.swift
+//  StarShop2
+//
+//  Created by Tatiana Magdalena on 25/01/18.
+//  Copyright © 2018 TOM. All rights reserved.
+//
+
+import Foundation
+
+struct Product: Decodable {
+    var title: String
+    var price: Int
+    var seller: String
+    var thumbnailHd: String
+    
+    func mapper() -> ProductPresentation {
+        return ProductPresentation(titleText: title, priceText: price.asCurrencyFormat, imageUrl: thumbnailHd)
+    }
+}
